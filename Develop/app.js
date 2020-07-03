a09d990b5ae9011e2d75c19c634ce53e1b2b8b79
Evan Bero <evanbero1980@gmail.com>
2020-07-02 23:51:10 -0400

    updated all js files and readme

--- Develop/app.js
+++ Develop/app.js
@@ -9,27 +9,122 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { start } = require("repl");
 
-
+const teamMembers = [];
+// let employeeData
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+init();
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function init(response) {
+    inquirer.prompt({
+        type: "checkbox",
+        name: "addEmployee",
+        message: "Would you like to add a new employee?",
+        choices: ["Yes", "No"]
+    }).then(function (response) {
+        console.log(response)
+        if (response.addEmployee[0] === "Yes") {
+            addTeamMember();
+        }
+        else {
+            createHTML()
+        }
+    })
+};
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function addTeamMember(answers) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter employee name: "
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Please enter employee ID: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter employee email: "
+        },
+        {
+            type: "checkbox",
+            name: "role",
+            message: "Which type of team member would you like to add?",
+            choices: ["Manager", "Engineer", "Intern", "I don't want to add any more teammembers"]
+        }
+    ]).then(function (answers) {
+        // employeeData = answers
+        if (answers.role[0] === "Engineer") {
+            createEngineer(answers);
+        }
+        else if (answers.role[0] === "Intern") {
+            createIntern(answers);
+        }
+        else if (answers.role[0] === "Manager") {
+            createManager(answers)
+        }
+        else if (answers.role[0] === "I don't want to add any more teammembers") {
+            createHTML();
+        }
+    })
+};
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function createEngineer(answers) {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "Please enter GitHub username: "
+        }
+    ]).then(function (github) {
+        const engineer = new Engineer(answers.name, answers.ID, answers.email, github.github)
+        teamMembers.push(engineer);
+        console.log(engineer)
+        init();
+    })
+
+};
+
+function createIntern(answers) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter intern school: "
+        }
+    ]).then(function (school) {
+        const intern = new Intern(answers.name, answers.ID, answers.email, school.school)
+        teamMembers.push(intern);
+        console.log(intern)
+        init();
+    })
+};
+
+function createManager(answers) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "office",
+            message: "Please enter office number: "
+        }
+    ]).then(function (office) {
+        const manager = new Manager(answers.name, answers.ID, answers.email, office.office)
+        teamMembers.push(manager);
+        console.log(manager)
+        init();
+    })
+};
+
+function createHTML() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    };
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# template-engine
+# Template Engine - Employee Summary
+Application developed for management to add employees to their team roster.
+
+# Description:
+Using node, user will add employee information. Once the user is finished adding employees,
+a team.html file is rendered, and can be found in the output folder. 
+When the team.html file is opened in the browser, all of the employees added will populate in the browser window.
+
+![Screen Shot 2020-07-02 at 11 36 17 PM](https://user-images.githubusercontent.com/63209633/86429782-889cb180-bcbe-11ea-8640-c3dad5e3af50.png)
+![Screen Shot 2020-07-02 at 11 36 37 PM](https://user-images.githubusercontent.com/63209633/86429790-8d616580-bcbe-11ea-9f73-0139b864e268.png)
+![Screen Shot 2020-07-02 at 11 38 18 PM](https://user-images.githubusercontent.com/63209633/86429803-918d8300-bcbe-11ea-966a-700cb86d0bbd.png)
+![Screen Shot 2020-07-02 at 11 38 43 PM](https://user-images.githubusercontent.com/63209633/86429816-9ce0ae80-bcbe-11ea-9526-d8cda14f2824.png)
+![Screen Shot 2020-07-02 at 11 44 15 PM](https://user-images.githubusercontent.com/63209633/86429832-a79b4380-bcbe-11ea-9886-c898c05e46f2.png)
+![Screen Shot 2020-07-02 at 11 45 43 PM](https://user-images.githubusercontent.com/63209633/86429839-abc76100-bcbe-11ea-87a8-62408c6f6ed6.png)
+![Screen Shot 2020-07-02 at 11 46 23 PM](https://user-images.githubusercontent.com/63209633/86429848-b124ab80-bcbe-11ea-816f-bab3078866d0.png)
+
+MIT License
+
+Copyright (c) 2020 Evan Bero
